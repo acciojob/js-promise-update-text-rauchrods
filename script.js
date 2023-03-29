6d46b11704d35d4785b9cff7006e6a055143a7c2
Rauch Rodrigues <94990103+rauchrods@users.noreply.github.com>
2023-03-29 23:30:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let prom = new Promise((resolve, reject) => {
+    setTimeout(()=>{
+        
+        resolve("Hello, world!");
+    },1000)
+   
+});
+
+prom.then((result) => {
+    document.querySelector("#output").innerText = result;
+}).catch((err) => {
+    
+});
